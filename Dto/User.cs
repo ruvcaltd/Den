31f00cebc5209054cb564c78776a328fd9f16715
ruvcaltd <53894177+ruvcaltd@users.noreply.github.com>
2020-06-14 14:02:32 +0100

    initial

---
+++ Dto/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Den.Dto
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+
+        public static IEnumerable<User> GetSomeData()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                yield return new User()
+                {
+                    FirstName = (Guid.NewGuid()).ToString(), LastName = Guid.NewGuid().ToString(),
+                    Email = Guid.NewGuid().ToString()
+                };
+            }
+        }
+    }
+}
---
+++ wwwroot/js/nav.js
@@ -0,0 +1,39 @@
+﻿class NavMenu {
+
+    
+
+    openNav(navId, mainBodyId, fullscreen) {
+
+        if (window.NavMenuOpen) return;
+       
+        if (!fullscreen) {
+            document.getElementById(navId).style.width = "700px";
+            document.getElementById(mainBodyId).style.marginLeft = "700px";
+            document.getElementById(mainBodyId).style.backgroundColor = "rgba(0,0,0,0.4)";
+
+            document.getElementById(mainBodyId)['onclick'] = () => this.closeNav(navId, mainBodyId);
+
+        } else {
+            document.getElementById(navId).style.width = "100%";
+        }
+
+        setTimeout(() => window.NavMenuOpen = true, 500);
+    }
+
+    closeNav(navId, mainBodyId) {
+        if (!window.NavMenuOpen) return;
+
+        console.log(navId);
+        console.log(navId);
+
+        document.getElementById(navId).style.width = "0";
+        document.getElementById(mainBodyId).style.marginLeft = "0";
+        document.getElementById(mainBodyId).style.backgroundColor = "white";
+
+        window.NavMenuOpen = false;
+    }
+}
+
+window.NavMenuOpen = false;
+window.NavMenu = new NavMenu();
+//alert(window.NavMenu);
